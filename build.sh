45c34b1757557c2da8b68aa36917ac2bfe9395e7
Zakariyya <zakariyya6840@gmail.com>
2025-10-22 19:38:46 +0400

    REMOVE HARDCODED LOGIN

--- build.sh
+++ build.sh
@@ -11,7 +11,3 @@ python manage.py migrate
 # Collect static files
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
-
-# Create superuser (will only create if env vars are set and user doesn't exist)
-echo "Creating superuser..."
-python manage.py createsu
